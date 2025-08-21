2f3b21611ff8c971d092b392dd69e2468a88caab
AligXVIII <tassada@rambler.ru>
2025-08-21 21:22:56 +0300

    f-строку поправил,поменял название переменных

--- tests_type/test_get_pet.py
+++ tests_type/test_get_pet.py
@@ -5,7 +5,7 @@ PET_SERVICE_URL = 'http://5.181.109.28:9090/api/v3'
 def test_successful_get_pet_id(create_and_delete_pet):
     pet_id = create_and_delete_pet['id']
     
-    response = requests.get(PET_SERVICE_URL+f'/pet/{pet_id}')
+    response = requests.get(f'{PET_SERVICE_URL}/pet/{pet_id}')
     response_json = response.json()
 
     assert response.status_code == 200
@@ -13,14 +13,14 @@ def test_successful_get_pet_id(create_and_delete_pet):
 
 def test_get_by_name_and_check_failed(create_and_delete_pet):
  
-    false_pet_id = create_and_delete_pet['name']
-    response = requests.get(f'{PET_SERVICE_URL}/pet/{false_pet_id}')
+    pet_name = create_and_delete_pet['name']
+    response = requests.get(f'{PET_SERVICE_URL}/pet/{pet_name}')
 
     assert response.status_code == 400
 
 def test_get_pet_by_nonexistent_id():
 
-    false_pet_id = 999999999
-    response = requests.get(f'{PET_SERVICE_URL}/pet/{false_pet_id}')
+    nonexistent_id = 999999999
+    response = requests.get(f'{PET_SERVICE_URL}/pet/{nonexistent_id}')
 
     assert response.status_code == 404
