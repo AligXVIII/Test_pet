63143d1a632f3887e40daaa8b62c881b070e3d73
AligXVIII <tassada@rambler.ru>
2025-08-21 21:05:48 +0300

    изменил название теста и переменную в тесте на удаление.первая попытка

--- tests_type/test_delete_pet.py
+++ tests_type/test_delete_pet.py
@@ -1,9 +1,7 @@
 import requests
 
-
 PET_SERVICE_URL = 'http://5.181.109.28:9090/api/v3'
-      
-
+    
 def test_deleted_pet(create_pet):
 
     DELETE_PET_ID = create_pet['id']
@@ -15,13 +13,13 @@ def test_deleted_pet(create_pet):
     assert requests_response.status_code == 404
 
 
-def test_invalid_pet_value(create_pet):
+def test_try_delete_pet_by_name(create_pet):
 
-    DELETE_PET_ID = create_pet['name']
+    PET_NAME = create_pet['name']
     TRUE_PET_ID = create_pet['id']
 
     
-    response = requests.delete(f'{PET_SERVICE_URL}/pet/{DELETE_PET_ID}')
+    response = requests.delete(f'{PET_SERVICE_URL}/pet/{PET_NAME}')
     assert response.status_code == 400
 
     requests_response = requests.get(f'{PET_SERVICE_URL}/pet/{TRUE_PET_ID}')
