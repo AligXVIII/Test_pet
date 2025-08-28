36a295301d408b7b45cddcf05b0e48f4ac8de804
AligXVIII <tassada@rambler.ru>
2025-08-28 20:27:37 +0300

    Добавил команды title и attach пока только в тесты на обновления данных(если все правильно,то также обновлю все остальные)

--- tests_type/test_put.py
+++ tests_type/test_put.py
@@ -1,10 +1,12 @@
 import requests
+import json
 import allure
 from pydantic import ValidationError
 from pet_schema import Pet 
 
 PET_SERVICE_URL = 'http://5.181.109.28:9090/api/v3'
-    
+
+@allure.title("Обновление данных питомца и проверка изменений")   
 def test_update_pet_information(create_and_delete_pet):
 
     with allure.step("Получить ID питомца для обновления и подготовить новые данные питомца"):
@@ -25,14 +27,26 @@ def test_update_pet_information(create_and_delete_pet):
             ],
             "photoUrls": ["https://i.ytimg.com/vi/vPPx29Co0vk/maxresdefault.jpg"]
         }
-
+        
+        allure.attach(
+            json.dumps(new_info_pet, ensure_ascii=False, indent=2),
+            "Данные для отправки",
+            allure.attachment_type.JSON
+        )
+    
     with allure.step("Отправить запрос для обновления информации и проверить обновление питомца"):
         update_response = requests.put(f'{PET_SERVICE_URL}/pet', json=new_info_pet)
         assert update_response.status_code == 200
+
+        allure.attach(
+                f"Status: {update_response.status_code}\nBody: {update_response.text}",
+            "Ответ сервера",
+                allure.attachment_type.TEXT
+            )
     
-        get_response = requests.get(f'{PET_SERVICE_URL}/pet/{ID_OF_THE_UPDATE_PET}')
-        assert get_response.status_code == 200
-        updated_data = get_response.json()
+    get_response = requests.get(f'{PET_SERVICE_URL}/pet/{ID_OF_THE_UPDATE_PET}')
+    assert get_response.status_code == 200
+    updated_data = get_response.json()
     
     with allure.step("Сравнить ответ с схемой Pet"):
         try:
@@ -49,38 +63,57 @@ def test_update_pet_information(create_and_delete_pet):
         assert validated_pet.tags[0].id == new_info_pet["tags"][0]["id"]
         assert validated_pet.tags[0].name == new_info_pet["tags"][0]["name"]    
 
-def test_invalid_specified_path_for_update():
+@allure.title('Неверный путь для обновления данных') 
+def test_invalid_specified_path_for_update(create_and_delete_pet):
+    
+    with allure.step("Указать неверный путь"):
+        update_response = requests.put(f'{PET_SERVICE_URL}/not_pet', json=create_and_delete_pet)    
+    
+    with allure.step("Получить ошибку: Питомец не найден"):
+        assert update_response.status_code == 404
+    
+        allure.attach(
+                f"Status: {update_response.status_code}",
+               "Ответ сервера",
+                allure.attachment_type.TEXT
+            )
 
-    new_info_pet = {
-        "id": 80,
-        "name": "Cat",
-        "status": "sold",
-    }
 
-    update_response = requests.put(f'{PET_SERVICE_URL}/not_pet', json=new_info_pet)    
-
-    assert update_response.status_code == 404
-
+@allure.title('Неккоректные данные для обновления') 
 def test_failed_update_due_to_an_invalid_pet_ID():
 
-    new_info_pet = {
+    with allure.step("Создаем питомца с неккоректным ID"):
+        new_info_pet = {
         "id": 'BC',
         "name": "Cat",
         "status": "sold",
     }
+    
+        allure.attach(
+            json.dumps(new_info_pet, ensure_ascii=False, indent=2),
+            "Данные с неккоректным ID",
+            allure.attachment_type.JSON
+        )
 
-    update_response = requests.put(f'{PET_SERVICE_URL}/pet', json=new_info_pet)    
+    with allure.step("Получаем ошибку о неверных данных"):
+        update_response = requests.put(f'{PET_SERVICE_URL}/pet', json=new_info_pet)    
+        assert update_response.status_code == 400
 
-    assert update_response.status_code == 400
+        allure.attach(
+             f"Status: {update_response.status_code}",
+            "Ответ сервера",
+             allure.attachment_type.TEXT
+        )
 
-def test_invalid_request_patch():
+@allure.title('Недопустимая команда обновления данных') 
+def test_invalid_request_patch(create_and_delete_pet):
+    with allure.step("Используем команду patch,недопустимый метод"):
+        update_response = requests.patch(f'{PET_SERVICE_URL}/pet', json=create_and_delete_pet) 
 
-    new_info_pet = {
-        "id": 80,
-        "name": "Cat",
-        "status": "sold",
-    }
-
-    update_response = requests.patch(f'{PET_SERVICE_URL}/pet', json=new_info_pet)    
-
-    assert update_response.status_code == 405
+    with allure.step("Получаем статус код о недопустимом HTTP-методе"):
+        assert update_response.status_code == 405
+        allure.attach(
+             f"Status: {update_response.status_code}",
+            "Ответ сервера",
+             allure.attachment_type.TEXT
+        )
